670025021423c3d07424dcfd6060e79215562112
midnightingale <45861449+midnightingale@users.noreply.github.com>
2019-10-09 07:56:12 -0700

    Update boardgen.js

--- boardgen.js
+++ boardgen.js
@@ -8,16 +8,16 @@ boardLength = 5; //test condition
 
 for(var y=0; y<boardLength; y++){
 
-	$('#space-container').append('<div>');
-	if (!board[y]) board[y] = new Array (boardLength);
+	$('#space-container').append('<div>'); //new row of spaces
+	if (!board[y]) board[y] = new Array (boardLength); 
 
 	for(var x=0; x<boardLength; x++){
-		if (!board[y][x]) board[y][x] = new Space(x,y);
+		if (!board[y][x]) board[y][x] = new Space(x,y); //creating a nested array
 		$('#space-container').append('<input id="space" type="button" class="space">');
 	}
-	$('#space-container').append('</div>');
+	$('#space-container').append('</div>'); //ends row of spaces
 	}
-	console.log(board);
+	//console.log(board); (useful for testing)
 })
 
 class Space {
