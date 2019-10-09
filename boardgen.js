1ae204126fc60c9a87d3e0280c1dfa40c7cbc8b4
midnightingale <45861449+midnightingale@users.noreply.github.com>
2019-10-09 08:31:10 -0700

    Change buttons to images

--- boardgen.js
+++ boardgen.js
@@ -2,14 +2,17 @@
 
 var boardLength;
 var board = []; //this is an array of objects
+//var flaggedSpaces = [];
+//var revealedBlank = []; //unsure if this and the above should actually be init
+var hammy = "http://bit.do/hammyboi"
 
 $('#newgame').click(function(){ 
 boardLength = 5; //test condition
 
 for(var y=0; y<boardLength; y++){
 
-	$('#space-container').append('<div>'); //new row of spaces
-	if (!board[y]) board[y] = new Array (boardLength); 
+	$('#space-container').append('<div>');
+	if (!board[y]) board[y] = new Array (boardLength);
 
 	for(var x=0; x<boardLength; x++){
 		if (!board[y][x]) board[y][x] = new Space(x,y); //creating a nested array
